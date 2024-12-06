1f4334d9c27a9e8144bc29c2d723447cdd64b0b1
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-12-06 10:18:05 +0000

    add %||% operator definition

--- R/utils_url.R
+++ R/utils_url.R
@@ -9,3 +9,12 @@ trim_url <- function(url) {
 trim_trailing_slash <- function(url) {
   sub(pattern = "/$", replacement = "", x = url, perl = TRUE)
 }
+
+`%||%` <- function(x, y) {
+  if (is_null(x)) y else x
+}
+
+# Reexport from base on newer versions of R
+if (exists("%||%", envir = .BaseNamespaceEnv)) {
+  `%||%` <- get("%||%", envir = .BaseNamespaceEnv)
+}
